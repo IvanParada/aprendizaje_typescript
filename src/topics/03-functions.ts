4b7f2ada09679a99d4346bad5cb608934ea3580a
Iván Parada <ivan.parada2001@alumnos.ubiobio.cl>
2024-03-16 22:52:06 -0300

    tarea objetos e interfaces

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -1,49 +1,49 @@
 
 
-function addNumbers(a: number, b: number) {
-    return a + b;
-}
+// function addNumbers(a: number, b: number) {
+//     return a + b;
+// }
 
-const addNumbersArrow = (a: number, b: number): string => {
-    return `${a + b}`;
-}
+// const addNumbersArrow = (a: number, b: number): string => {
+//     return `${a + b}`;
+// }
 
-function multiply(firstNumber: number, secondNumber?: number, base: number = 2) {
-    return firstNumber * base;
-}
-
-
-// const result: number = addNumbers(1, 2)
-// const ArrowResult: string = addNumbersArrow(1, 2)
-// const multiplyResult: number = multiply(5)
-// console.log({ result, ArrowResult, multiplyResult })
-
-interface Character {
-    name: string;
-    hp: number;
-    showHp: () => void;
-}
+// function multiply(firstNumber: number, secondNumber?: number, base: number = 2) {
+//     return firstNumber * base;
+// }
 
 
-const healCharacter = (character: Character, amount: number  ) => {
+// // const result: number = addNumbers(1, 2)
+// // const ArrowResult: string = addNumbersArrow(1, 2)
+// // const multiplyResult: number = multiply(5)
+// // console.log({ result, ArrowResult, multiplyResult })
 
-    character.hp += amount;
-
-}
-
-const strider : Character = {
-    name: 'Strider',
-    hp: 50,
-    showHp() {
-        console.log(`Puntos de vida ${this.hp}`);
-    }
-}
-
-healCharacter(strider, 10)
-strider.showHp()
-healCharacter(strider, 30)
+// interface Character {
+//     name: string;
+//     hp: number;
+//     showHp: () => void;
+// }
 
 
-strider.showHp()
+// const healCharacter = (character: Character, amount: number  ) => {
 
-export { };
+//     character.hp += amount;
+
+// }
+
+// const strider : Character = {
+//     name: 'Strider',
+//     hp: 50,
+//     showHp() {
+//         console.log(`Puntos de vida ${this.hp}`);
+//     }
+// }
+
+// healCharacter(strider, 10)
+// strider.showHp()
+// healCharacter(strider, 30)
+
+
+// strider.showHp()
+
+// export { };
